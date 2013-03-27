3781c57dc11e3cfe10f17ba15ac94b3f6602e735
Benedikt Pletzer <bpletzer@googlemail.com>
2013-03-27 22:21:53 +0100

    gettin on

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
+// ~ tilde
+
 var rewrite = require("connect-url-rewrite");
 var connect = require("connect");
 var semver = require("semver");
 
 var rules = [
-  "^\/example.html /redirected.html"
+  //"^\/example.html /redirected.html"
 ]
 
+var deployed = [
+		'1.0.0',
+		'1.0.1',
+		'1.1.0',
+		'2.0.0'
+	];
+
 var app = connect()
-  .use(function(req, res){
-    var version = req.url.slice(1);
-    console.log(version, semver.satisfies('2.0.1', version));
+  .use(function(req, res, next){
+
+	var version = req.url.split('/')[1];
+	console.log('valid version: ' + semver.valid(version), 'valid range: ' + semver.validRange(version))
+    
+	var index = deployed.length - 1;
+	for (index; index>=0; index--){
+		if (semver.satisfies(deployed[index], version)) {
+			break;
+		}
+	} 
+	
+	if (index === -1) {
+		index = deployed.length -1;
+	}
+	console.log(index)
+	console.log(deployed[index])
+	
+	rules.push("^/"+version+" /"+deployed[index]);
+	
+	next();
   })
   .use(rewrite(rules))
-  .use(connect.logger('dev'))
+  .use(function(req, res, next){
+	console.dir(rules)
+	console.log(req.url)
+    next();
+  })	
   .use(connect.static('public'))
-  .use(function(req, res){
-    res.end('hello world\n' + console.dir(req.url));
-  })
- .listen(3000);
+  .listen(3000);
