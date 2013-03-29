c4bf636d3592fefb3f5e2997dc0afcbcbc1d0fc4
Benedikt Pletzer <bpletzer@googlemail.com>
2013-03-29 13:37:53 +0100

    set of commands

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // ~ tilde
 var connect = require("connect");
 
+var appFolder = __dirname+'/public/get/';
+
 var app = connect()
-  .use("/get", require("./filter/semverRewrite")(__dirname+'/public/get/'))
+	.use("/get", require("./filter/semverRewrite")(appFolder))
+
+	.use("/list", require("./versions").list(appFolder))
+	
+	.use("/active", require("./versions").active(appFolder))
+	
+	.use("/activate", require("./versions").activate(appFolder))
+	
+	.use("/deactivate", require("./versions").deactivate(appFolder))
 
   .use(function(req, res, next){
 
--- versions.js
+++ versions.js
@@ -12,5 +12,71 @@ var versions = function(root){
 
 		return folders;	
 	};
+	
+var list = function(root) {
+	
+	return function(req, res){
+	
+		var app = req.url.split('/')[1];
+	
+		res.end(versions(root+app).toString())
+	}
+}
+
+var active = function(root) {
+	
+	return function(req, res){
+	
+		var app = req.url.split('/')[1];
+		var active = JSON.parse(fs.readFileSync(root+app+'/active.json'));
+	
+		res.end(active.toString())
+	}
+}
+
+var activate = function (root) {
+	
+	return function(req, res){
+	
+		var app = req.url.split('/')[1];
+		var version = req.url.split('/')[2];
+		var active = JSON.parse(fs.readFileSync(root+app+'/active.json'));
+		var found = false;
+		
+		active.forEach(function(v){
+			if (v === version) found = true;
+		})
+		
+		if (!found) {
+			active.push(version);
+			fs.writeFile(root+app+'/active.json', JSON.stringify(active));
+		}
+	
+		res.end(active.toString())
+	}
+}
+
+var deactivate = function (root) {
+	
+	return function(req, res){
+	
+		var app = req.url.split('/')[1];
+		var version = req.url.split('/')[2];
+		var active = JSON.parse(fs.readFileSync(root+app+'/active.json'));
+		
+		active = active.filter(function(v){
+			
+			return (v !== version);
+		})
+		
+		fs.writeFile(root+app+'/active.json', JSON.stringify(active));
+	
+		res.end(active.toString())
+	}
+}
 
 module.exports.get = versions;
+module.exports.list = list;
+module.exports.active = active;
+module.exports.activate = activate;
+module.exports.deactivate = deactivate;
